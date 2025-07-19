5a2d80fb6e16e0f151ed7dfa9ed27521a5b263c5
mad6717 <mad6717@gmail.com>
2025-07-19 17:43:45 +0300

    Models for the user and the notes are made

---
+++ models/Note.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Note', noteSchema)
