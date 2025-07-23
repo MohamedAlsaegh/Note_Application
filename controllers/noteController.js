14e9abea38318ab2dda87e90a36c3030ef0a43f9
mad6717 <mad6717@gmail.com>
2025-07-23 09:47:22 +0300

    Styling nav,sign-in,sign-up with bootstrab

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -47,6 +47,12 @@ const updateNoteById = async (req, res) => {
     req.body.isCompleted = !!req.body.isCompleted
     req.body.tag = req.body.tag.trim().toLowerCase()
     await Note.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    if (req.body.isCompleted === 'on') {
+      req.body.isCompleted = true
+    } else {
+      req.body.isCompleted = false
+    }
     res.redirect('/notes/show')
   } catch (error) {
     console.error('Note update error:', error.message)
