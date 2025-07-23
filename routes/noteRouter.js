ba3bfadc2b028b3c5474c168b5e4b8d6127831b4
mad6717 <mad6717@gmail.com>
2025-07-23 10:26:33 +0300

    foreach fas reaking the page issue solved

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -5,17 +5,12 @@ const isSignedIn = require('../middleware/is-signed-in')
 const noteController = require('../controllers/noteController.js')
 
 //ApI's
-router.get('/show',noteController.notes_create_get)
-
-router.post('/', noteController.createNote)
-router.get('/', noteController.getAllnotes)
-
 router.get('/show', noteController.note_show_get)
+router.post('/', noteController.notes_create_post)
+router.get('/', noteController.getAllnotes)
 router.get('/:id/edit', noteController.noteEdit)
-
 router.get('/:id', noteController.getnoteById)
 router.put('/:id', noteController.updateNoteById)
-
 router.delete('/:id', noteController.deleteNoteById)
 
 module.exports = router
